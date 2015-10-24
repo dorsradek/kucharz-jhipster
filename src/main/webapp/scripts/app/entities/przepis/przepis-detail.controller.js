dcc9b68ab289ea50ae6b9173712878773bfe4daa
rdors <dors.radek@gmail.com>
2015-10-25 00:25:06 +0200

    Poprawa wyswietlania prezpisow

--- src/main/webapp/scripts/app/entities/przepis/przepis-detail.controller.js
+++ src/main/webapp/scripts/app/entities/przepis/przepis-detail.controller.js
@@ -1,16 +1,12 @@
 'use strict';
 
 angular.module('kucharzApp')
-    .controller('PrzepisDetailController', function ($scope, $state, $rootScope, $stateParams, entity, entityImage, Przepis, KategoriaPrzepisu, PracochlonnoscPrzepisu, PrzepisProdukt) {
+    .controller('PrzepisDetailController', function ($scope, $state, $rootScope, $stateParams, entity, Przepis, KategoriaPrzepisu, PracochlonnoscPrzepisu, PrzepisProdukt) {
         $scope.przepis = entity;
-        $scope.dupa = entityImage;
         $scope.state = $state;
         $scope.load = function (id) {
             Przepis.get({id: id}, function (result) {
                 $scope.przepis = result;
-            });
-            PrzepisImage.get({id: id}, function (result) {
-                $scope.dupa = result;
             });
         };
         $rootScope.$on('kucharzApp:przepisUpdate', function (event, result) {
@@ -32,6 +28,5 @@ angular.module('kucharzApp')
                     }).modal('hide');
                 });
         };
-        ó
 
     });
